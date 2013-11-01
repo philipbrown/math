a322ff11edfaead7df7f60b22878caf0c7b7fbcb
Philip Brown <phil@ipbrown.com>
2013-11-01 14:23:43 +0000

    Add docblock to add method

--- src/Philipbrown/Math/Math.php
+++ src/Philipbrown/Math/Math.php
@@ -2,6 +2,14 @@
 
 class Math {
 
+  /**
+   * Add
+   *
+   * @param string $a
+   * @param string $a
+   * @param integer $scale
+   * @return string
+   */
   public function add($a, $b, $scale = 0)
   {
     return bcadd($a, $b, $scale);
