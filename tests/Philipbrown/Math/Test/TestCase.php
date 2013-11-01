c27ed40a6399333a8e96c2e69a0e167a4644cdbc
Philip Brown <phil@ipbrown.com>
2013-11-01 14:09:12 +0000

    Set up test stuff

---
+++ tests/Philipbrown/Math/Test/TestCase.php
@@ -0,0 +1,6 @@
+<?php namespace Philipbrown\Math\Test;
+
+use PHPUnit_Framework_TestCase;
+use Philipbrown\Math\Math;
+
+class TestCase extends PHPUnit_Framework_TestCase {}
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// Get autoloader
+$autoloader = require __DIR__.'/../vendor/autoload.php';
+
+// Add Math
+$autoloader->add('Philipbrown\Math', __DIR__);
